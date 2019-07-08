f5681221b6d3cfc65a64f4532030270c344ec987
Matt McNabb <mmcnabb@outlook.com>
2019-07-07 22:34:16 -0400

    :sparkles:  Add Get-OktaApplication (#7)

--- src/functions/Get-OktaApplication.ps1
+++ src/functions/Get-OktaApplication.ps1
@@ -1,1 +1,62 @@
-# get all apps, or apps for a user
+function Get-OktaApplication
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, ParameterSetName = "Identity")]
+        [string]
+        $Identity,
+
+        [Parameter(Mandatory, ParameterSetName = "Filter")]
+        [string]
+        $Filter,
+
+        [Parameter(Mandatory, ParameterSetName = "All")]
+        [switch]
+        $All,
+
+        [Parameter(Mandatory, ParameterSetName = "User", ValueFromPipeline)]
+        [Okta.PS.User]
+        $User
+    )
+
+    process
+    {
+        try
+        {
+            $Task = switch ($PSCmdlet.ParameterSetName)
+            {
+                "Identity"
+                {
+                    [Okta.PS.Application]::GetApplication($Script:Moduleclient, $Identity)
+                }
+
+                "Filter"
+                {
+                    [Okta.PS.Application]::FilterApplications($Script:Moduleclient, $Filter)
+                }
+
+                "User"
+                {
+                    [Okta.PS.Application]::FilterApplications($Script:Moduleclient, "user.id eq `"$($User.Id)`"")
+                }
+
+                "All"
+                {
+                    [Okta.PS.Application]::GetAllApplications($Script:Moduleclient)
+                }
+            }
+
+            $Result = Wait-Task -Task $Task
+
+            foreach ($Item in $Result)
+            {
+                [Okta.PS.Application]::new($Item)
+            }
+        }
+        catch
+        {
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
+}
